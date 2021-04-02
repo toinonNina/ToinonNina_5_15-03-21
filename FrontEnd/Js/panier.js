8a226399912646ff00090b24d62572ff6019a69c
nina <zd.toinon@gmail.com>
2021-04-02 15:10:01 +0200

    paufinement panier

--- FrontEnd/Js/panier.js
+++ FrontEnd/Js/panier.js
@@ -1,7 +1,6 @@
 //déclaration de la variable dans laquelle on met les key et les values qui sont dans le local storage
 let showproductLocalStorage = JSON.parse(localStorage.getItem('product'));
 //json.parse pour convertir les données au format json qui sont dans le localStorage en objet Javascript
-console.log(showproductLocalStorage);
 
 //affichage des produits du panier
 const showBasket = document.querySelector("#monpanier");
@@ -23,12 +22,14 @@ if (showproductLocalStorage === null || showproductLocalStorage == 0) {
         showProductBasket = showProductBasket + `
         <tr >
         <td scope="row"><img class="th-img" src="${showproductLocalStorage[j].imageUrl}"></td>
-        <td><h2 class ="titre-panier panierh2">${showproductLocalStorage[j].name}</h2> <h3 class ="titre-panier panierh3">${showproductLocalStorage[j].option_product}</h3></td>
+        <td><h2 class ="titreNom panierh2">${showproductLocalStorage[j].name}</h2></td>
+        <td><div class="counter"><div class ="down" > - </div><input type="text" value="1"><div class="up"> + </div></div></td>
         <td><h4 class ="titre-panier panierh4">${showproductLocalStorage[j].price}€</h4></td>
-        <td>${showproductLocalStorage[j].quantity}</td>
         <td class ="delete"><button class = "btn btn-primary btn-delete "><i class="fas fa-trash-alt"></i></button></td>
     </tr>
         `;
+        const sousTotal = document.querySelector(".sous_Total");
+        sousTotal.innerHTML = calculTotal() + " €";
     }
     if (j == showproductLocalStorage.length) {
         showBasketnotempty.innerHTML = showProductBasket;
@@ -52,14 +53,8 @@ const btn_delete = document.querySelectorAll(".btn-delete");
 for (let k = 0; k < btn_delete.length; k++) {
     btn_delete[k].addEventListener("click", (event) => {
         event.preventDefault();
-
         let id_select_delete = showproductLocalStorage[k].id;
-        let color_select_delete = showproductLocalStorage[k].option_product;
-        console.log('id_select_delete');
-        console.log(id_select_delete);
         showproductLocalStorage = showproductLocalStorage.filter(element => element.id !== id_select_delete);
-        console.log(showproductLocalStorage);
-
         localStorage.setItem("product", JSON.stringify(showproductLocalStorage));
         alert("Produit Retirer de votre panier");
         window.location.href = "panier.html";
@@ -67,21 +62,15 @@ for (let k = 0; k < btn_delete.length; k++) {
 }
 
 // price égale quantité fois prix
+
 // Sous total = +eachprix
-let teddyPrice = JSON.parse(localStorage.getItem('product'));
 
-let total = function calculTotal() {
-        let teddyPriceArr = [];
-        for (l = 0; l < teddyPrice.length; l++) {
-            teddyPriceArr.push(teddyPrice[l].price);
 
-        }
-        let totalAmount = teddyPriceArr.reduce((total, current) => total + current, 0);
-        return totalAmount;
-
+function calculTotal() {
+    let teddyPriceArr = [];
+    for (l = 0; l < showproductLocalStorage.length; l++) {
+        teddyPriceArr.push(showproductLocalStorage[l].price);
     }
-    ();
-
-localStorage.setItem('total', total);
-const sousTotal = document.querySelector(".sous_Total");
-sousTotal.innerHTML = total + " €";
+    let totalAmount = teddyPriceArr.reduce((total, current) => total + current, 0);
+    return totalAmount;
+};
